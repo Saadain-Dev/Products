cc4e49cb3a4aa8747ec6caa7dd8c789e81a7588c
Saadain Shaikh <hussainsaadain@gmail.com>
2024-09-07 15:07:28 +0500

    solve

--- Script.js
+++ Script.js
@@ -1,11 +1,17 @@
+
+// let navBtn = document.getElementById("nav-btn");
+// let mobileNav = document.getElementById("nav");
+
+// navBtn.onclick = () => {
+//   mobileNav.style.maxWidth = mobileNav.style.maxWidth ? null : mobileNav.scrollWidth + "px";
+// };
+
 const cardContainer = document.querySelector(".card");
 const productCard = ({ title, img, price, desc }) => {
     return `
         <div class="w-full max-w-sm border rounded-lg">
             <a href="#">
-            <div class="bg-white mb-3">
             <img class="w-full h-[200px] object-contain" src="${img}" alt="" />
-            </div>
             </a>
             <div class="px-3 pb-3 flex justify-between">
                 <a href="#"><h5 class="text-lg font-semibold line-clamp-1">${title}</h5></a>
@@ -23,24 +29,23 @@ const productCardSkeleton = () => {
         <div class="w-1/2 h-6 bg-gray-300 rounded"></div>
         <div class="w-1/4 h-6 bg-gray-300 rounded"></div>
     </div>
-</div>
-
-    `;
+</div>`;
 };
 
-
-
-let productList = [];
+const length = document.querySelector(".Product-length");
+let productList = [] ;
+length.innerHTML = productList.length
 const renderData = (data, container, fun) => {
     container.innerHTML = `${data.map(fun).join("")}`;
 };
 
 const fetchDataByApi = async () => {
-    renderData([1,1,1,1,1,1],cardContainer,productCardSkeleton);
+    renderData([1, 1, 1, 1, 1, 1], cardContainer, productCardSkeleton);
     let { data } = await axios.get("https://66d70f85006bfbe2e64fa810.mockapi.io/products");
-    if(data.length == 0) {
+    if (data.length == 0) {
         cardContainer.innerHTML = "<h1 class='col-span-full text-2xl'>No Products Available...</h1>"
-    }else{
+    } else {
+        
         productList = data
         renderData(data, cardContainer, productCard);
     }
@@ -50,11 +55,10 @@ fetchDataByApi()
 
 const form = document.querySelector("form");
 const searchProducts = document.querySelector(".inp");
-const Search = (e)=>{
-  e.preventDefault();
-  let inputValue = searchProducts.value.toLowerCase();
-  let filteredProducts = productList.filter(item => item.title.toLowerCase().includes(inputValue));
-  renderData(filteredProducts, cardContainer, productCard);
+const Search = (e) => {
+    e.preventDefault();
+    let inputValue = searchProducts.value.toLowerCase();
+    let filteredProducts = productList.filter(item => item.title.toLowerCase().includes(inputValue));
+    renderData(filteredProducts, cardContainer, productCard);
 }
 form.addEventListener("submit", Search);
-    
